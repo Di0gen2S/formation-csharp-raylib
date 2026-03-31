57d4f9c6757ca8916522a200c0612a597b409883
Di0gen2S <csharp_learning@outlook.com>
2026-03-31 22:14:22 +0200

    feat: premier projet Raylib - fenetre Hello World

---
+++ HelloRaylib/Program.cs
@@ -0,0 +1,14 @@
+﻿using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "Hello, Raylib!");
+Raylib.SetTargetFPS(60);
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.RayWhite);
+    Raylib.DrawText("Hello, Raylib!", 240, 270, 48, Color.DarkBlue);
+    Raylib.EndDrawing();
+}
+
+Raylib.CloseWindow();
