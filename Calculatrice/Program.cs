e36d18fe73ddfd3ab7001c4ab289b30ce46cf3fe
Di0gen2S <csharp_learning@outlook.com>
2026-04-01 12:49:55 +0200

    feat: calculatrice console - session 1.1

---
+++ Calculatrice/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("=== Calculatrice ===");
+
+// Saisie du premier nombre
+Console.Write("Entre le premier nombre : ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+// Saisie du deuxième nombre
+Console.Write("Entre le deuxième nombre : ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+// Calculs et affichage
+Console.WriteLine("--- Résultats ---");
+Console.WriteLine(a + " + " + b + " = " + (a + b));
+Console.WriteLine(a + " - " + b + " = " + (a - b));
+Console.WriteLine(a + " * " + b + " = " + (a * b));
+
+// Cas spécial : division par zéro
+if (b == 0)
+{
+    Console.WriteLine("Division impossible (division par zéro !)");
+}
+else
+{
+    Console.WriteLine(a + " / " + b + " = " + (a / b));
+}
